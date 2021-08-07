a3ce506591d079803ade94b754c8012de04fbafb
Yestin Vastenouw <yj.vastenouw@student.avans.nl>
2021-08-07 14:41:24 +0200

    Made spectator command available for everyone.

--- src/essence.game/qpang/chat/command/commands/ReloadShopCommand.h
+++ src/essence.game/qpang/chat/command/commands/ReloadShopCommand.h
@@ -16,7 +16,9 @@ public:
 	void handle(std::shared_ptr<Player> player, const std::vector<std::u16string>& args)
 	{
 		auto shopManager = Game::instance()->getShopManager();
+
 		shopManager->initialize();
+
 		const auto items = Game::instance()->getShopManager()->list();
 		
 		Game::instance()->send(ShopItems(items));
