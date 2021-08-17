da8d703f0b9364efdb8aa251c56a934c3c768cfd
Yestin Vastenouw <yj.vastenouw@student.avans.nl>
2021-08-18 01:54:07 +0200

    Added the implementation for extending cards.

--- src/essence.game/packets/lobby/incoming/inventory/ExtendCardEvent.h
+++ src/essence.game/packets/lobby/incoming/inventory/ExtendCardEvent.h
@@ -1,24 +1,104 @@
 #pragma once
 
+#include "CardExtended.h"
+#include "CardPurchaseComplete.h"
+#include "OpenGiftSuccess.h"
+#include "UpdateCashBalance.h"
 #include "core/communication/packet/PacketEvent.h"
 
 class ExtendCardEvent final : public PacketEvent
 {
 public:
-	void handle(QpangConnection::Ptr conn, QpangPacket& packet) override
+	void handle(const QpangConnection::Ptr conn, QpangPacket& packet) override
 	{
-		auto cardId = packet.readLong();
-		auto itemId = packet.readInt();
-		auto idk2 = packet.readFlag();
-		auto price = packet.readInt();
-		auto seqId = packet.readInt();
-		auto flag = packet.readFlag();
-		auto kek = packet.readShort();
+		const auto cardId = packet.readLong();
+		const auto itemId = packet.readInt();
 
-		//auto card = conn->getPlayer()->getInventoryManager()->get(cardId);
-		//
-		//card.period = 10;
+		const auto payType = packet.readFlag() ? 1 : 0; // Paytype (don or cash), true = cash, false = don;
+		const auto isCash = payType == 1;
 
-		//conn->send(CardExtended(card, 100, true));
+		const auto price = packet.readInt();
+		const auto seqId = packet.readInt();
+
+		const auto unkFlag1 = packet.readFlag();
+		const auto unkShort1 = packet.readShort();
+
+		const auto player = conn->getPlayer();
+
+		// Ensure the player exists.
+		if (player == nullptr)
+		{
+			return;
+		}
+
+		const auto squarePlayer = player->getSquarePlayer();
+
+		if (squarePlayer == nullptr)
+		{
+			player->broadcast(u"You may only renew items from the park.");
+
+			return;
+		}
+
+		const auto playerHasCard = player->getInventoryManager()->hasCard(cardId);
+		const auto cardIsExpired = player->getInventoryManager()->isExpired(cardId);
+		const auto cardIsEquipped = player->getEquipmentManager()->hasEquipped(cardId);
+
+		std::cout << "Has card: " << playerHasCard << "\n";
+		std::cout << "Is expired: " << cardIsExpired << "\n";
+		std::cout << "Is equipped: " << cardIsEquipped << "\n";
+		std::cout << "\n";
+
+		if (!playerHasCard || !cardIsExpired || cardIsEquipped)
+		{
+			return;
+		}
+
+		// You may only extend a card if it currently exists in the shop, so cancel if it does not exist.
+		if (!Game::instance()->getShopManager()->exists(seqId))
+		{
+			return;
+		}
+
+		const auto shopItem = Game::instance()->getShopManager()->get(seqId);
+
+		if (shopItem.soldCount >= shopItem.stock && shopItem.stock != 9999)
+		{
+			player->broadcast(u"You may not renew this item since it is out of stock.");
+
+			return;
+		}
+
+		// Validate that the values from the packet are the same as the shop in the database.
+		if ((shopItem.isCash != isCash) || (shopItem.itemId != itemId) || (shopItem.price != price))
+		{
+			return;
+		}
+
+		// Ensure the player has sufficient funds to pay for the item.
+		if (const auto hasSufficientFunds = (shopItem.isCash)
+			? player->getCash() >= shopItem.price
+			: player->getDon() >= shopItem.price; !hasSufficientFunds)
+		{
+			std::cout << "Player does not have sufficient funds" << "\n";
+			std::cout << "Cash: " << player->getCash() << "Don: " << player->getDon() << "\n";
+			std::cout << "Price: " << shopItem.price << "\n";
+
+			return;
+		}
+
+		std::cout << "Player has sufficient funds" << "\n";
+		std::cout << "Cash: " << player->getCash() << "Don: " << player->getDon() << "\n";
+		std::cout << "Price: " << shopItem.price << "\n";
+
+
+		player->getInventoryManager()->deleteCard(cardId);
+
+		const auto boughtInventoryCard = Game::instance()->getShopManager()->buy(player, seqId, false);
+
+		conn->send(CardExtended(boughtInventoryCard, shopItem.isCash ? player->getCash() : player->getDon(), isCash));
+		conn->send(UpdateCashBalance(player->getCash()));
+
+		player->send(OpenGiftSuccess(player, boughtInventoryCard));
 	}
-};	
+};
