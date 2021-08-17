0504922fb2beb852f0d37e299fac315e81fed527
Yestin Vastenouw <yj.vastenouw@student.avans.nl>
2021-08-17 12:58:02 +0200

    Refactored equipment packets.

--- src/essence.game/packets/lobby/outgoing/equipment/EquippedSkillCards.h
+++ src/essence.game/packets/lobby/outgoing/equipment/EquippedSkillCards.h
@@ -9,10 +9,14 @@
 class EquippedSkillCards : public LobbyServerPacket
 {
 public:
-	EquippedSkillCards(const std::array<InventoryCard, 3>& skillCards) : LobbyServerPacket(647)
+	explicit EquippedSkillCards(const std::array<InventoryCard, 3>& skillCards) : LobbyServerPacket(647)
 	{
 		for (size_t i = 0; i < 5; i++)
-			for (const InventoryCard& card : skillCards)
-				InventoryCardWriter::write(this, card);
+		{
+			for (const InventoryCard& skillCard : skillCards)
+			{
+				InventoryCardWriter::write(this, skillCard);
+			}
+		}
 	}
 };
