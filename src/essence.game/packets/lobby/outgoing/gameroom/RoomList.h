ef950cc30c784c6bb635880750b263f078eb6c1f
Yestin Vastenouw <yj.vastenouw@student.avans.nl>
2021-08-17 13:01:54 +0200

    Refactored RoomList packet.

--- src/essence.game/packets/lobby/outgoing/gameroom/RoomList.h
+++ src/essence.game/packets/lobby/outgoing/gameroom/RoomList.h
@@ -4,21 +4,22 @@
 
 #include "packets/LobbyServerPacket.h"
 #include "packets/writers/RoomWriter.h"
-
 #include "qpang/room/Room.h"
 
 class RoomList : public LobbyServerPacket
 {
 public:
-	RoomList(const std::vector<Room::Ptr>& rooms) : LobbyServerPacket(759)
+	explicit RoomList(const std::vector<Room::Ptr>& rooms) : LobbyServerPacket(759)
 	{
-		auto size = static_cast<uint16_t>(rooms.size());
+		const auto roomSize = static_cast<uint16_t>(rooms.size());
 
-		writeShort(size);
-		writeShort(size);
-		writeShort(size);
+		writeShort(roomSize);
+		writeShort(roomSize);
+		writeShort(roomSize);
 
 		for (const auto& room : rooms)
+		{
 			RoomWriter::write(this, room);
+		}
 	}
 };
