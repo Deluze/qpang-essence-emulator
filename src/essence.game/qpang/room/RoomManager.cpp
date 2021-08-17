a4d93603fb5b3bba46adab9a95fca164b2b5bc33
Yestin Vastenouw <yj.vastenouw@student.avans.nl>
2021-08-17 16:28:57 +0200

    Revert "Revert "Refactored room manager.""
    This reverts commit 09db11859a9972845ea80a437e407f2a5cc89714.

--- src/essence.game/qpang/room/RoomManager.cpp
+++ src/essence.game/qpang/room/RoomManager.cpp
@@ -2,7 +2,6 @@
 
 #include "core/Emulator.h"
 #include "core/config/ConfigManager.h"
-
 #include "qpang/Game.h"
 #include "qpang/room/Room.h"
 
@@ -13,25 +12,29 @@ RoomManager::RoomManager()
 
 void RoomManager::tick()
 {
-	std::lock_guard<std::recursive_mutex> lg(m_roomMx);
+	std::lock_guard lg(m_roomMx);
 
 	for (const auto& [id, room] : m_rooms)
+	{
 		room->tick();
+	}
 }
 
 std::vector<std::shared_ptr<Room>> RoomManager::list()
 {
-	std::lock_guard<std::recursive_mutex> lg(m_roomMx);
+	std::lock_guard lg(m_roomMx);
 
 	std::vector<std::shared_ptr<Room>> rooms;
 
 	for (const auto& [id, room] : m_rooms)
+	{
 		rooms.push_back(room);
+	}
 
 	return rooms;
 }
 
-std::shared_ptr<Room> RoomManager::create(std::u16string name, uint8_t map, uint8_t mode)
+std::shared_ptr<Room> RoomManager::create(const std::u16string& name, uint8_t map, uint8_t mode)
 {
 	uint32_t host = inet_addr(CONFIG_MANAGER->getString("HOST").c_str());
 	uint16_t port = static_cast<uint16_t>(CONFIG_MANAGER->getInt("GAME_PORT"));
@@ -42,37 +45,39 @@ std::shared_ptr<Room> RoomManager::create(std::u16string name, uint8_t map, uint
 	m_roomMx.lock();
 	m_rooms[id] = room;
 	m_roomMx.unlock();
-	
+
 	return room;
 }
 
-void RoomManager::remove(uint32_t id)
+void RoomManager::remove(const uint32_t id)
 {
-	std::lock_guard<std::recursive_mutex> lg(m_roomMx);
+	std::lock_guard lg(m_roomMx);
 
 	m_rooms.erase(id);
 }
 
 Room::Ptr RoomManager::get(uint32_t id)
 {
-	std::lock_guard<std::recursive_mutex> lg(m_roomMx);
+	std::lock_guard lg(m_roomMx);
 
 	auto it = m_rooms.find(id);
 
 	if (it == m_rooms.cend())
+	{
 		return nullptr;
+	}
 
 	return it->second;
 }
 
-GameModeManager* RoomManager::getGameModeManager()
+GameModeManager* RoomManager::getGameModeManager() const
 {
 	return m_gameModeManager;
 }
 
 uint32_t RoomManager::getAvailableRoomId()
 {
-	std::lock_guard<std::recursive_mutex> lg(m_roomMx);
+	std::lock_guard lg(m_roomMx);
 
 	uint32_t id{ 1 };
 
@@ -81,7 +86,9 @@ uint32_t RoomManager::getAvailableRoomId()
 		const auto it = m_rooms.find(id);
 
 		if (it == m_rooms.cend())
+		{
 			break;
+		}
 
 		id++;
 	}
