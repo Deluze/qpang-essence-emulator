87bfe4c54de31d4db6ac0a458d15eb307f4e2cf5
Yestin Vastenouw <yj.vastenouw@student.avans.nl>
2021-08-17 16:29:18 +0200

    Revert "Revert "Refactored a bunch of the Room code according to new conventions.""
    This reverts commit e5296e9f940a688534739fe12949a858d9c0901e.

--- src/essence.game/qpang/room/Room.h
+++ src/essence.game/qpang/room/Room.h
@@ -1,10 +1,10 @@
 #pragma once
 
-#include <memory>
-#include <unordered_map>
 #include <cstdint>
-#include <string>
+#include <memory>
 #include <mutex>
+#include <string>
+#include <unordered_map>
 
 #include "GameConnection.h"
 #include "RoomPlayer.h"
@@ -17,16 +17,17 @@ class GameConnection;
 class Room : public std::enable_shared_from_this<Room>
 {
 public:
+	// ReSharper disable once CppInconsistentNaming
 	using Ptr = std::shared_ptr<Room>;
 
-	Room(const uint32_t id, const std::u16string name, const uint8_t map, const uint8_t mode, const uint32_t host, const uint16_t port);
+	Room(uint32_t id, std::u16string name, uint8_t map, uint8_t mode, uint32_t host, uint16_t port);
 
 	void addPlayer(GameConnection* conn);
 	void removePlayer(uint32_t id);
 	std::shared_ptr<RoomPlayer> getPlayer(uint32_t id);
-	std::shared_ptr<RoomSession> getRoomSession();
+	std::shared_ptr<RoomSession> getRoomSession() const;
 
-	void tick();
+	void tick() const;
 
 	void start();
 	void close();
@@ -74,50 +75,55 @@ public:
 	bool isMeleeOnly() const;
 	void setMeleeOnly(bool meleeOnly);
 
-	uint32_t getMasterId();
+	uint32_t getMasterId() const;
 	uint32_t findNewMaster();
 
 	uint8_t getAvailableTeam();
 	bool isTeamAvailable(uint8_t team);
 
-	uint32_t getScorePoints();
+	uint32_t getScorePoints() const;
 	void setScorePoints(uint32_t points);
 
-	uint32_t getScoreTime();
+	uint32_t getScoreTime() const;
 	void setScoreTime(uint32_t minutes);
 
-	bool isPointsGame();
+	bool isPointsGame() const;
 	void setIsPointsGame(bool isPoints);
 
-	bool isPlaying();
+	bool isPlaying() const;
 	void unreadyAll(bool notify = false);
 
-	std::string getPassword();
+	std::string getPassword() const;
 	void setPassword(std::string& password);
 
-	bool isEventRoom();
+	bool isEventRoom() const;
 	void setEventRoom(bool isEventRoom = false);
 
-	bool isReloadGlitchEnabled();
+	bool isReloadGlitchEnabled() const;
 	void setIsReloadGlitchEnabled(bool isReloadGlitchEnabled = false);
 
 	template<typename T, typename... Args>
 	void broadcast(Args... args)
 	{
-		std::lock_guard<std::recursive_mutex> lg(m_playerMx);
+		std::lock_guard lg(m_playerMx);
 
 		for (const auto& [id, roomPlayer] : m_players)
 			roomPlayer->getConnection()->postNetEvent(new T(args...));
 	}
 
 	template<typename T, typename... Args>
+
 	void broadcastWaiting(Args... args)
 	{
-		std::lock_guard<std::recursive_mutex> lg(m_playerMx);
+		std::lock_guard lg(m_playerMx);
 
 		for (const auto& [id, roomPlayer] : m_players)
+		{
 			if (!roomPlayer->isPlaying())
+			{
 				roomPlayer->getConnection()->postNetEvent(new T(args...));
+			}
+		}
 	}
 
 private:
