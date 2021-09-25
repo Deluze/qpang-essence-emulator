0eb2d275121f69dc174d4e8543a69d43732c4ca5
YestinJarrett <y.vastenouw@gmail.com>
2021-09-25 19:42:48 +0200

    Implemented teamchat for team games and public enemy mode.

--- src/essence.game/qpang/room/tnl/net_events/client/cg_mesg.hpp
+++ src/essence.game/qpang/room/tnl/net_events/client/cg_mesg.hpp
@@ -8,11 +8,15 @@ class CGMesg : public GameNetEvent
 {
 	typedef NetEvent Parent;
 public:
-
 	CGMesg() : GameNetEvent{ CG_MESG, NetEvent::GuaranteeType::Guaranteed, NetEvent::DirClientToServer }
 	{
 		nicknameBuffer = new ByteBuffer();
 		messageBuffer = new ByteBuffer();
+	};
+
+	enum CMD : uint16_t {
+		ALL_CHAT = 0,
+		TEAM_CHAT = 8
 	};
 
 	void pack(EventConnection* conn, BitStream* bstream) {};
@@ -45,7 +49,7 @@ public:
 
 			if (roomPlayer->isSpectating() && !message.empty()) {
 				auto rank = roomPlayer->getPlayer()->getRank();
-				
+
 				if (rank == 1) { // player
 					return player->broadcast(u"You may not chat as a spectator.");
 				}
@@ -55,13 +59,61 @@ public:
 				return;
 
 			if (roomSession == nullptr)
+			{
 				room->broadcast<GCMesg>(player->getId(), cmd, player->getName(), message);
+			}
 			else
 			{
 				if (roomPlayer->isPlaying())
-					roomSession->relay<GCMesg>(player->getId(), cmd, player->getName(), message);
+				{
+					switch (cmd)
+					{
+					case TEAM_CHAT:
+						if (roomSession->getGameMode()->isPublicEnemyMode())
+						{
+							const auto currentlySelectedTag = roomSession->getCurrentlySelectedTag();
+							const auto isPublicEnemy = currentlySelectedTag == player->getId();
+
+							if (isPublicEnemy)
+							{
+								// Player is public enemy so only send message to other public enemies.
+								// In our case, there exists only one public enemy so only send the message to the player itsself.
+								roomPlayer->send(new GCMesg(player->getId(), cmd, player->getName(), message));
+							}
+							else 
+							{
+								// Player is not a public enemy, so send the message to every player except the public enemy.
+								roomSession->relayExcept<GCMesg>(currentlySelectedTag, player->getId(), cmd, player->getName(), message);
+							}
+						}
+						else
+						{
+							// If we get here this means a team chat message was sent, so lets make sure the gamemode is a team mode.
+							if (!roomSession->getGameMode()->isTeamMode())
+							{
+								return;
+							}
+
+							// Now we know it's a team chat message, send the message to only team players.
+							const auto teamPlayers = roomSession->getPlayersForTeam(roomPlayer->getTeam());
+
+							for (const auto &teamPlayer : teamPlayers)
+							{
+								teamPlayer->post(new GCMesg(player->getId(), cmd, player->getName(), message));
+							}
+						}
+
+						break;
+					case ALL_CHAT:
+					default:
+						roomSession->relay<GCMesg>(player->getId(), cmd, player->getName(), message);
+						break;
+					}
+				}
 				else
+				{
 					room->broadcastWaiting<GCMesg>(player->getId(), cmd, player->getName(), message);
+				}
 			}
 		}
 	}
