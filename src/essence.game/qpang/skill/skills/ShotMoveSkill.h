7ac98660f590e647d238fa32c162fc6b644ffa65
YestinJarrett <y.vastenouw@gmail.com>
2021-10-13 19:52:15 +0200

    Added a base class for Skill and added a custom skill called ShotMoveSkill.

---
+++ src/essence.game/qpang/skill/skills/ShotMoveSkill.h
@@ -0,0 +1,9 @@
+#pragma once
+
+#include "qpang/skill/skills/Skill.h"
+
+class ShotMoveSkill : public Skill
+{
+public:
+private:
+};
---
+++ src/essence.game/qpang/skill/skills/Skill.h
@@ -0,0 +1,7 @@
+#pragma once
+
+class Skill
+{
+public:
+private:
+}
