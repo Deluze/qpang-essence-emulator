557aee49327412e187863df3f81053020ac83d4d
YestinJarrett <y.vastenouw@gmail.com>
2021-10-13 19:51:57 +0200

    Reworked SkillManager, just the basics for now.

--- src/essence.game/qpang/skill/SkillManager.cpp
+++ src/essence.game/qpang/skill/SkillManager.cpp
@@ -1,9 +1,23 @@
 #include "SkillManager.h"
 
+#include <qpang/skill/skills/ShotMoveSkill.h>
+
 SkillManager::SkillManager()
 {
 }
 
 void SkillManager::initialize()
 {
+	m_skills.clear();
+
+	m_skills[GameMode::DM][SkillID::SHOT_MOVE] = std::make_shared<ShotMoveSkill>();
+	m_skills[GameMode::TDM][SkillID::SHOT_MOVE] = std::make_shared<ShotMoveSkill>();
+	m_skills[GameMode::PTE][SkillID::SHOT_MOVE] = std::make_shared<ShotMoveSkill>();
+	m_skills[GameMode::VIP][SkillID::SHOT_MOVE] = std::make_shared<ShotMoveSkill>();
+	m_skills[GameMode::PREY][SkillID::SHOT_MOVE] = std::make_shared<ShotMoveSkill>();
+}
+
+std::unordered_map<uint32_t, std::shared_ptr<Skill>> SkillManager::getSkillsForGameMode(uint8_t mode)
+{
+	return m_skills[mode];
 }
--- src/essence.game/qpang/skill/SkillManager.h
+++ src/essence.game/qpang/skill/SkillManager.h
@@ -1,16 +1,24 @@
 #pragma once
 
-#include <memory>
+#include <cstdint>
 #include <unordered_map>
-#include <functional>
 
-class Skill;
+#include <qpang/room/game_mode/GameMode.h>
+#include <qpang/skill/skills/Skill.h>
 
 class SkillManager
 {
 public:
+	enum SkillID : uint32_t
+	{
+		SHOT_MOVE = 1258356738
+	};
+
 	SkillManager();
 
 	void initialize();
+
+	std::unordered_map<uint32_t, std::shared_ptr<Skill>> getSkillsForGameMode(uint8_t mode);
 private:
+	std::unordered_map<uint8_t, std::unordered_map<uint32_t, std::shared_ptr<Skill>>> m_skills;
 };
