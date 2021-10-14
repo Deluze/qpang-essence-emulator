0a4a6a7d37f8376f22b50f75b9f2198c385b09db
YestinJarrett <y.vastenouw@gmail.com>
2021-10-14 23:28:34 +0200

    Added Rage skill.

--- src/essence.game/qpang/skill/SkillManager.cpp
+++ src/essence.game/qpang/skill/SkillManager.cpp
@@ -8,6 +8,7 @@
 #include <qpang/skill/skills/PocketSizeSkill.h>
 #include <qpang/skill/skills/ChaosSkill.h>
 #include <qpang/Game.h>
+#include <qpang/skill/skills/RageSkill.h>
 
 SkillManager::SkillManager()
 {
@@ -22,30 +23,35 @@ void SkillManager::initialize()
 	m_skills[GameMode::DM][ItemID::SKILL_CAMO] = []() { return std::make_unique<CamoSkill>(); };
 	m_skills[GameMode::DM][ItemID::SKILL_POCKET_SIZE] = []() { return std::make_unique<PocketSizeSkill>(); };
 	m_skills[GameMode::DM][ItemID::SKILL_CHAOS] = []() { return std::make_unique<ChaosSkill>(); };
+	m_skills[GameMode::DM][ItemID::SKILL_RAGE] = []() { return std::make_unique<RageSkill>(); };
 
-	m_skills[GameMode::TDM][ItemID::SKILL_RAPID_FIRE] = []() { return std::make_unique<RapidFireSkill>(); };
-	m_skills[GameMode::TDM][ItemID::SKILL_HAWK_EYE] = []() { return std::make_unique<HawkEyeSkill>(); };
-	m_skills[GameMode::TDM][ItemID::SKILL_CAMO] = []() { return std::make_unique<CamoSkill>(); };
-	m_skills[GameMode::TDM][ItemID::SKILL_POCKET_SIZE] = []() { return std::make_unique<PocketSizeSkill>(); };
+	//m_skills[GameMode::TDM][ItemID::SKILL_RAPID_FIRE] = []() { return std::make_unique<RapidFireSkill>(); };
+	//m_skills[GameMode::TDM][ItemID::SKILL_HAWK_EYE] = []() { return std::make_unique<HawkEyeSkill>(); };
+	//m_skills[GameMode::TDM][ItemID::SKILL_CAMO] = []() { return std::make_unique<CamoSkill>(); };
+	//m_skills[GameMode::TDM][ItemID::SKILL_POCKET_SIZE] = []() { return std::make_unique<PocketSizeSkill>(); };
 	m_skills[GameMode::TDM][ItemID::SKILL_CHAOS] = []() { return std::make_unique<ChaosSkill>(); };
+	m_skills[GameMode::TDM][ItemID::SKILL_RAGE] = []() { return std::make_unique<RageSkill>(); };
 
 	m_skills[GameMode::PTE][ItemID::SKILL_RAPID_FIRE] = []() { return std::make_unique<RapidFireSkill>(); };
 	m_skills[GameMode::PTE][ItemID::SKILL_HAWK_EYE] = []() { return std::make_unique<HawkEyeSkill>(); };
 	m_skills[GameMode::PTE][ItemID::SKILL_CAMO] = []() { return std::make_unique<CamoSkill>(); };
 	m_skills[GameMode::PTE][ItemID::SKILL_POCKET_SIZE] = []() { return std::make_unique<PocketSizeSkill>(); };
 	m_skills[GameMode::PTE][ItemID::SKILL_CHAOS] = []() { return std::make_unique<ChaosSkill>(); };
+	m_skills[GameMode::PTE][ItemID::SKILL_RAGE] = []() { return std::make_unique<RageSkill>(); };
 
 	m_skills[GameMode::VIP][ItemID::SKILL_RAPID_FIRE] = []() { return std::make_unique<RapidFireSkill>(); };
 	m_skills[GameMode::VIP][ItemID::SKILL_HAWK_EYE] = []() { return std::make_unique<HawkEyeSkill>(); };
 	m_skills[GameMode::VIP][ItemID::SKILL_CAMO] = []() { return std::make_unique<CamoSkill>(); };
 	m_skills[GameMode::VIP][ItemID::SKILL_POCKET_SIZE] = []() { return std::make_unique<PocketSizeSkill>(); };
 	m_skills[GameMode::VIP][ItemID::SKILL_CHAOS] = []() { return std::make_unique<ChaosSkill>(); };
+	m_skills[GameMode::VIP][ItemID::SKILL_RAGE] = []() { return std::make_unique<RageSkill>(); };
 
 	m_skills[GameMode::PREY][ItemID::SKILL_RAPID_FIRE] = []() { return std::make_unique<RapidFireSkill>(); };
 	m_skills[GameMode::PREY][ItemID::SKILL_HAWK_EYE] = []() { return std::make_unique<HawkEyeSkill>(); };
 	m_skills[GameMode::PREY][ItemID::SKILL_CAMO] = []() { return std::make_unique<CamoSkill>(); };
 	m_skills[GameMode::PREY][ItemID::SKILL_POCKET_SIZE] = []() { return std::make_unique<PocketSizeSkill>(); };
 	m_skills[GameMode::PREY][ItemID::SKILL_CHAOS] = []() { return std::make_unique<ChaosSkill>(); };
+	m_skills[GameMode::PREY][ItemID::SKILL_RAGE] = []() { return std::make_unique<RageSkill>(); };
 }
 
 std::unordered_map<uint32_t, std::function<std::unique_ptr<Skill>()>> SkillManager::getSkillsForGameMode(uint8_t mode)
---
+++ src/essence.game/qpang/skill/skills/RageSkill.h
@@ -0,0 +1,23 @@
+#pragma once
+
+#include <iostream>
+
+class RageSkill : public Skill
+{
+public:
+	RageSkill() : Skill()
+	{
+		m_requiredSkillPoints = 1;
+
+		m_hasDuration = true;
+		m_durationInSeconds = 15;
+
+		m_skillTarget = SkillTarget::ENEMY_PLAYER;
+	}
+
+	uint32_t getItemId() override
+	{
+		return ItemID::SKILL_RAGE;
+	}
+private:
+};
