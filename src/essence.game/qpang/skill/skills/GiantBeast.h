6a7dfafb8ddd3f9a30e86457533661f8798c8e64
YestinJarrett <y.vastenouw@gmail.com>
2021-10-15 12:19:33 +0200

    Added some todo's for later.

--- src/essence.game/qpang/skill/skills/GiantBeast.h
+++ src/essence.game/qpang/skill/skills/GiantBeast.h
@@ -19,13 +19,18 @@ public:
 	{
 		Skill::onApply();
 
+		// TODO: Save health before transforming.
+		// TODO: Save currently held weapon before transforming.
+		// TODO: Change weapon to weapon for zilla.
+		// 
 		// Set the health of the player to 600.
 		m_player->setHealth(600);
 	}
 
 	void onWearOff() override
 	{
-
+		// TODO: Set health back to what it was?
+		// TODO: Set weapon back to previous weapon.
 	}
 
 	uint32_t getItemId() override
