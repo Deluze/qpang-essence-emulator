7f044704d24b7a1d48e8a53abbbd77f29f9cf112
YestinJarrett <y.vastenouw@gmail.com>
2021-11-04 13:47:28 +0100

    Added some logs to the packet handler to find out more about packets (for now).

--- src/essence.core/core/communication/PacketHandler.cpp
+++ src/essence.core/core/communication/PacketHandler.cpp
@@ -8,8 +8,26 @@ void PacketHandler::handle(QpangConnection::Ptr conn, QpangPacket pack)
 	const auto it = m_events.find(pack.getPacketId());
 
 	if (it == m_events.cend())
-		return;
+	{
+#ifdef WIN32
+		HANDLE handle = GetStdHandle(STD_OUTPUT_HANDLE);
 
+		SetConsoleTextAttribute(handle, 12);
+#endif
+		std::cout << "PacketHandler::handle >> Received an UNKNOWN packet with ID " << pack.getPacketId() << std::endl;
+
+		return;
+	}
+
+#ifdef WIN32
+	HANDLE handle = GetStdHandle(STD_OUTPUT_HANDLE);
+
+	SetConsoleTextAttribute(handle, 10);
+#endif
+	std::cout << "PacketHandler::handle >> Handling packet with ID " << pack.getPacketId() << std::endl;
+#ifdef WIN32
+	SetConsoleTextAttribute(handle, FOREGROUND_BLUE | FOREGROUND_RED | FOREGROUND_GREEN);
+#endif
 	const bool connAuthorized = conn->getPlayer() != nullptr;
 	const bool packProtected = !it->second->isUnauthorizedPacket();
 
