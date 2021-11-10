bf6635c814836f165dc0a530aab182d1136c0453
YestinJarrett <y.vastenouw@gmail.com>
2021-11-10 19:26:44 +0100

    Started doing some research on trading.

---
+++ src/essence.game/packets/lobby/incoming/trading/RequestTrade.h
@@ -0,0 +1,32 @@
+#pragma once
+
+#include "core/communication/packet/PacketEvent.h"
+
+class RequestTrade final : public PacketEvent
+{
+public:
+	// Trade cancel 884
+	void handle(const QpangConnection::Ptr conn, QpangPacket& packet) override
+	{
+		const auto player = conn->getPlayer();
+
+		const auto targetPlayerId = packet.readInt();
+		const auto targetPlayer = Game::instance()->getPlayer(targetPlayerId);
+
+		if (targetPlayer == nullptr)
+		{
+			// TODO: Send trade fail?
+			// 877 - Trade error "The trading process has stopped because of error caused in the system internally"?
+			conn->send(LobbyServerPacket(877));
+
+			return;
+		}
+
+		std::cout << "RequestTrade::handle >> Player " << player->getId() << " has requested to trade with " << targetPlayerId << std::endl;
+
+		// 876 - Shows trade request menu.
+		conn->send(LobbyServerPacket(876));
+
+		//targetPlayer->send(LobbyServerPacket(881)); // The other side has cancelled the request.
+	}
+};
