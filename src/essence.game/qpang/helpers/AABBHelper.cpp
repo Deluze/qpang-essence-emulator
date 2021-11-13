e7c95da85e3e5f054a9b8e223a4a7b3f5e295260
YestinJarrett <y.vastenouw@gmail.com>
2021-11-13 21:20:06 +0100

    Added a method which can determine if a position is in the radius of a spawn.

--- src/essence.game/qpang/helpers/AABBHelper.cpp
+++ src/essence.game/qpang/helpers/AABBHelper.cpp
@@ -1,5 +1,6 @@
 #include "AABBHelper.h"
 #include "Position.h"
+#include "Spawn.h"
 
 // ReSharper disable once CppInconsistentNaming
 bool AABBHelper::isPositionInBetweenBounds(const Position point, const Position minBound, const Position maxBound)
@@ -17,6 +18,17 @@ bool AABBHelper::isPositionInBetweenBounds(const Position point, const Position 
 		: isInRange(point.z, minBound.z, maxBound.z));
 
 	return (xPositionInBetweenBounds && yPositionInBetweenBounds && zPositionInBetweenBounds);
+}
+
+bool AABBHelper::IsPositionWithinSpawnRadius(const Position position, const Spawn spawn, const float radius)
+{
+	const Spawn spawnMin{ spawn.x - radius, spawn.y - radius, spawn.z - radius };
+	const Spawn spawnMax{ spawn.x + radius, spawn.y + radius, spawn.z + radius };
+
+	const auto xIsInRange = (position.x >= spawnMin.x && position.x <= spawnMax.x);
+	const auto zIsInRange = (position.z >= spawnMin.z && position.z <= spawnMax.z);
+
+	return (xIsInRange && zIsInRange);
 }
 
 bool AABBHelper::isNegative(const float value)
--- src/essence.game/qpang/helpers/AABBHelper.h
+++ src/essence.game/qpang/helpers/AABBHelper.h
@@ -1,12 +1,14 @@
 #pragma once
 
 #include "Position.h"
+#include "Spawn.h"
 
 // ReSharper disable once CppInconsistentNaming
 class AABBHelper
 {
 public:
 	static bool isPositionInBetweenBounds(const Position point, const Position minBound, const Position maxBound);
+	static bool IsPositionWithinSpawnRadius(Position position, Spawn spawn, float radius);
 private:
 	static bool isNegative(const float value);
 	static bool isInRange(const float value, const float min, const float max);
