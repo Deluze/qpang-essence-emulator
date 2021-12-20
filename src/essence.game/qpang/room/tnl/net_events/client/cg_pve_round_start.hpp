d6c4530562d3ce0ec115104fc2dc5a6ab9181bff
YestinJarrett <y.vastenouw@gmail.com>
2021-12-20 17:36:53 +0100

    Add basic implementation for CGPvERoundStart.

--- src/essence.game/qpang/room/tnl/net_events/client/cg_pve_round_start.hpp
+++ src/essence.game/qpang/room/tnl/net_events/client/cg_pve_round_start.hpp
@@ -2,17 +2,27 @@
 #define CG_PVE_ROUND_START_H
 
 #include "GameNetEvent.h"
+#include "gc_pve_new_round.hpp"
+
 class CGPvERoundStart : public GameNetEvent
 {
 	typedef NetEvent Parent;
 public:
-	CGPvERoundStart() : GameNetEvent{ CG_PVE_ROUND_START, NetEvent::GuaranteeType::GuaranteedOrdered, NetEvent::DirAny } {};
+	CGPvERoundStart() : GameNetEvent{ CG_PVE_ROUND_START, GuaranteedOrdered, DirClientToServer} {}
 
-	void pack(EventConnection* conn, BitStream* bstream) {};
-	void unpack(EventConnection* conn, BitStream* bstream) {};
-	void process(EventConnection* ps) 
+	void pack(EventConnection* conn, BitStream* bstream) override {}
+	void unpack(EventConnection* conn, BitStream* bstream) override {}
+
+	void handle(GameConnection* conn, const Player::Ptr player) override
 	{
-	};
+		// TODO: More checks need to happen here, but for now.. relay the new round event.
+		player->getRoomPlayer()->getRoomSessionPlayer()->getRoomSession()->relayPlaying<GCPvENewRound>();
+	}
+
+	void process(EventConnection* ps) override
+	{
+		post<CGPvERoundStart>(ps);
+	}
 
 	TNL_DECLARE_CLASS(CGPvERoundStart);
 };
