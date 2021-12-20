f7c00165faefeba9562208cdd2387b7129304c46
Hinnie <hsibma02@gmail.com>
2021-12-20 18:46:55 +0100

    Add GCPvEObjectInit and GCPvEObjectMove

--- src/essence.game/qpang/room/tnl/net_events/server/gc_pve_object_init.hpp
+++ src/essence.game/qpang/room/tnl/net_events/server/gc_pve_object_init.hpp
@@ -8,8 +8,34 @@ class GCPvEObjectInit : public GameNetEvent
 {
 	typedef NetEvent Parent;
 public:
-	GCPvEObjectInit() : GameNetEvent{ GC_PVE_OBJECT_INIT, NetEvent::GuaranteeType::GuaranteedOrdered, NetEvent::DirAny } {};
-	void pack(EventConnection* conn, BitStream* bstream) {};
+	U32 objectType; // 88
+	U32 objectUid; // 92
+	F32 xPosition; // 96
+	F32 yPosition; // 100
+	F32 zPosition; // 104
+	U16 unk_01;  // 108
+
+	GCPvEObjectInit() : GameNetEvent{ GC_PVE_OBJECT_INIT, NetEvent::GuaranteeType::GuaranteedOrdered, NetEvent::DirServerToClient } {};
+	GCPvEObjectInit(U32 objectType, U32 objectUid, F32 xPosition, F32 yPosition, F32 zPosition, U16 unk_01) : GameNetEvent{ GC_PVE_OBJECT_INIT, NetEvent::GuaranteeType::GuaranteedOrdered, NetEvent::DirServerToClient }
+	{
+		this->objectType = objectType;
+		this->objectUid = objectUid;
+		this->xPosition = xPosition;
+		this->yPosition = yPosition;
+		this->zPosition = zPosition;
+		this->unk_01 = unk_01;
+	};
+
+	void pack(EventConnection* conn, BitStream* bstream) 
+	{
+		bstream->write(objectType);
+		bstream->write(objectUid);
+		bstream->write(xPosition);
+		bstream->write(yPosition);
+		bstream->write(zPosition);
+		bstream->write(unk_01);
+	};
+
 	void unpack(EventConnection* conn, BitStream* bstream) {};
 	void process(EventConnection* ps) {};
 	TNL_DECLARE_CLASS(GCPvEObjectInit);
--- src/essence.game/qpang/room/tnl/net_events/server/gc_pve_object_move.hpp
+++ src/essence.game/qpang/room/tnl/net_events/server/gc_pve_object_move.hpp
@@ -8,9 +8,32 @@ class GCPvEObjectMove : public GameNetEvent
 {
 	typedef NetEvent Parent;
 public:
-	GCPvEObjectMove() : GameNetEvent{ GC_PVE_OBJECT_MOVE, NetEvent::GuaranteeType::GuaranteedOrdered, NetEvent::DirAny } {};
+	U32 objectUid;
+	F32 positionX;
+	F32 positionY;
+	F32 positionZ;
+	U32 unk_01;
 
-	void pack(EventConnection* conn, BitStream* bstream) {};
+	GCPvEObjectMove() : GameNetEvent{ GC_PVE_OBJECT_MOVE, NetEvent::GuaranteeType::GuaranteedOrdered, NetEvent::DirServerToClient } {};
+
+	GCPvEObjectMove(U32 objectUid, F32 positionX, F32 positionY, F32 positionZ, U32 speed) : GameNetEvent{ GC_PVE_OBJECT_MOVE, NetEvent::GuaranteeType::GuaranteedOrdered, NetEvent::DirServerToClient }
+	{
+		this->objectUid = objectUid;
+		this->positionX = positionX;
+		this->positionY = positionY;
+		this->positionZ = positionZ;
+		this->unk_01 = speed;
+	};
+
+	void pack(EventConnection* conn, BitStream* bstream) 
+	{
+		bstream->write(objectUid);
+		bstream->write(positionX);
+		bstream->write(positionY);
+		bstream->write(positionZ);
+		bstream->write(unk_01);
+	};
+
 	void unpack(EventConnection* conn, BitStream* bstream) {};
 	void process(EventConnection* ps) {};
 
