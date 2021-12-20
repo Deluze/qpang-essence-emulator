82d2c57186e4c622ad8ed23c8fca083dc923ef1d
YestinJarrett <y.vastenouw@gmail.com>
2021-12-20 01:58:31 +0100

    Add basic implementation for gc_pve_move_npc and gc_pve_move_nodes_npc.

--- src/essence.game/qpang/room/tnl/net_events/server/gc_pve_move_nodes_npc.hpp
+++ src/essence.game/qpang/room/tnl/net_events/server/gc_pve_move_nodes_npc.hpp
@@ -7,11 +7,29 @@ class GCPvEMoveNodesNpc : public GameNetEvent
 {
 	typedef NetEvent Parent;
 public:
-	GCPvEMoveNodesNpc() : GameNetEvent{ GC_PVE_MOVE_NODES_NPC, NetEvent::GuaranteeType::GuaranteedOrdered, NetEvent::DirAny } {};
+	U32 npcUid;
+	U32 nodeCount = 0; // Node count?
 
-	void pack(EventConnection* conn, BitStream* bstream) {};
-	void unpack(EventConnection* conn, BitStream* bstream) {};
-	void process(EventConnection* ps) {};
+	GCPvEMoveNodesNpc() : GameNetEvent{ GC_PVE_MOVE_NODES_NPC, GuaranteedOrdered, DirAny } {}
+	GCPvEMoveNodesNpc(const uint32_t npcUid) : GameNetEvent{ GC_PVE_MOVE_NODES_NPC, GuaranteedOrdered, DirAny},
+		npcUid(npcUid)
+	{
+	}
+
+	void pack(EventConnection* conn, BitStream* bstream)
+	{
+		bstream->write(npcUid);
+		bstream->write(nodeCount);
+
+		for (uint32_t i = 0; i < nodeCount; i++)
+		{
+			bstream->write(static_cast<U16>(0));
+			bstream->write(static_cast<U16>(0));
+		}
+	}
+
+	void unpack(EventConnection* conn, BitStream* bstream) {}
+	void process(EventConnection* ps){}
 
 	TNL_DECLARE_CLASS(GCPvEMoveNodesNpc);
 };
--- src/essence.game/qpang/room/tnl/net_events/server/gc_pve_move_npc.hpp
+++ src/essence.game/qpang/room/tnl/net_events/server/gc_pve_move_npc.hpp
@@ -7,11 +7,30 @@ class GCPvEMoveNpc : public GameNetEvent
 {
 	typedef NetEvent Parent;
 public:
-	GCPvEMoveNpc() : GameNetEvent{ GC_PVE_MOVE_NPC, NetEvent::GuaranteeType::GuaranteedOrdered, NetEvent::DirAny } {};
+	U32 npcUid;
+	U16 unk_02;
+	U16 unk_03;
 
-	void pack(EventConnection* conn, BitStream* bstream) {};
-	void unpack(EventConnection* conn, BitStream* bstream) {};
-	void process(EventConnection* ps) {};
+	GCPvEMoveNpc() : GameNetEvent{ GC_PVE_MOVE_NPC, GuaranteedOrdered, DirServerToClient } {}
+
+	GCPvEMoveNpc(const uint32_t npcUid, const uint16_t unk_02, const uint16_t unk_03)
+		: GameNetEvent{ GC_PVE_MOVE_NPC, GuaranteedOrdered, DirServerToClient },
+		npcUid(npcUid),
+		unk_02(unk_02),
+		unk_03(unk_03)
+	{
+	}
+
+	void pack(EventConnection* conn, BitStream* bstream)
+	{
+		bstream->write(npcUid);
+
+		bstream->write(unk_02);
+		bstream->write(unk_03);
+	}
+
+	void unpack(EventConnection* conn, BitStream* bstream) {}
+	void process(EventConnection* ps) {}
 
 	TNL_DECLARE_CLASS(GCPvEMoveNpc);
 };
