6bb5f27cea4ff1cf92656c2cb0e52222d82083e3
Hinnie <hsibma02@gmail.com>
2021-12-21 00:20:34 +0100

    Spawn second escalator

--- src/essence.game/qpang/room/game_mode/modes/PlayerVsEnvironment.cpp
+++ src/essence.game/qpang/room/game_mode/modes/PlayerVsEnvironment.cpp
@@ -3,7 +3,7 @@
 #include "Room.h"
 #include "RoomSession.h"
 
-#include "RedBoardObject.h"
+#include "EscalatorObject.h"
 
 #include <qpang/room/tnl/net_events/server/gc_pve_object_init.hpp>
 #include <qpang/room/tnl/net_events/server/gc_pve_object_move.hpp>
@@ -33,7 +33,8 @@ void PlayerVsEnvironment::onApply(std::shared_ptr<Room> room)
 void PlayerVsEnvironment::onStart(std::shared_ptr<RoomSession> roomSession)
 {
 	// 1st red board
-	roomSession->getObjectManager()->spawnObject(std::make_unique<RedBoardObject>(eRedBoardActionId::STAGE0_PIT1_BOARD1, Position { -13.64f, -0.5f, -22.64f }));
+	roomSession->getObjectManager()->spawnObject(std::make_unique<EscalatorObject>(eRedBoardActionId::STAGE0_PIT1_ESCALATOR1, Position { -13.64f, -0.5f, -22.64f }));
+	roomSession->getObjectManager()->spawnObject(std::make_unique<EscalatorObject>(eRedBoardActionId::STAGE0_PIT2_ESCALATOR1, Position{ 14.39f, -0.5f, -23.45f }));
 
 	GameMode::onStart(roomSession);
 }
