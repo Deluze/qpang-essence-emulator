fa1e34451476d126e854442366251f5ba48f3e29
YestinJarrett <y.vastenouw@gmail.com>
2021-12-20 15:43:53 +0100

    Add base implementation for CGPvEGetItem (item pickup).

--- src/essence.game/qpang/room/tnl/net_events/client/cg_pve_get_item.hpp
+++ src/essence.game/qpang/room/tnl/net_events/client/cg_pve_get_item.hpp
@@ -2,24 +2,38 @@
 #define CG_PVE_GET_ITEM_H
 
 #include "GameNetEvent.h"
+#include "gc_game_item.hpp"
+
 class CGPvEGetItem : public GameNetEvent
 {
 	typedef NetEvent Parent;
 public:
-	CGPvEGetItem() : GameNetEvent{ CG_PVE_GET_ITEM, NetEvent::GuaranteeType::GuaranteedOrdered, NetEvent::DirAny } {};
+	CGPvEGetItem() : GameNetEvent{ CG_PVE_GET_ITEM, GuaranteedOrdered, DirClientToServer} {}
 
-	U32 unk_01;
-	U32 unk_02;
+	U32 unk_01; // 88
+	U32 itemUid; // 92
 
-	void pack(EventConnection* conn, BitStream* bstream) {};
+	void pack(EventConnection* conn, BitStream* bstream) {}
+
 	void unpack(EventConnection* conn, BitStream* bstream) 
 	{
 		bstream->read(&unk_01);
-		bstream->read(&unk_02);
-	};
+		bstream->read(&itemUid);
+	}
+
+	void handle(GameConnection* conn, Player::Ptr player) override
+	{
+		std::cout << "CGPvEGetItem pickup item unk_01: " << unk_01 << ", ItemUid: " << itemUid << std::endl;
+
+		const auto roomSession = player->getRoomPlayer()->getRoomSessionPlayer()->getRoomSession();
+
+		roomSession->relayPlaying<GCGameItem>(GCGameItem::CMD::PICKUP_GAME_ITEM, player->getId(), 1191182352, itemUid, 0);
+	}
+
 	void process(EventConnection* ps) 
 	{
-	};
+		post<CGPvEGetItem>(ps);
+	}
 
 	TNL_DECLARE_CLASS(CGPvEGetItem);
 };
