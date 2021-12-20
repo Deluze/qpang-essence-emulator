6a635d8c0e537f4b69ef971026c4612eaf17fe77
YestinJarrett <y.vastenouw@gmail.com>
2021-12-20 16:48:21 +0100

    Update cg_move_report so the position gets set for every report.

--- src/essence.game/qpang/room/tnl/net_events/client/cg_move_report.hpp
+++ src/essence.game/qpang/room/tnl/net_events/client/cg_move_report.hpp
@@ -8,29 +8,61 @@ class CGMoveReport : public GameNetEvent
 public:
 
 	U32 playerId; //88
-	F32 xPosition; //92
-	F32 yPosition; //96
-	F32 zPosition; //100
+	F32 xPos; //92
+	F32 yPos; //96
+	F32 zPos; //100
 
 	// pitch & yaw?
 	F32 unk_05; //104
 	F32 unk_06; //108
 
-	CGMoveReport() : GameNetEvent{ CG_MOVE_REPORT, NetEvent::GuaranteeType::GuaranteedOrdered, NetEvent::DirClientToServer } {};
+	CGMoveReport() : GameNetEvent{ CG_MOVE_REPORT, GuaranteedOrdered, DirClientToServer } {}
 
-	void pack(EventConnection* conn, BitStream* bstream) {};
+	void pack(EventConnection* conn, BitStream* bstream) {}
+
 	void unpack(EventConnection* conn, BitStream* bstream)
 	{
 		bstream->read(&playerId);
-		bstream->read(&xPosition);
-		bstream->read(&yPosition);
-		bstream->read(&zPosition);
+		bstream->read(&xPos);
+		bstream->read(&yPos);
+		bstream->read(&zPos);
 		bstream->read(&unk_05);
 		bstream->read(&unk_06);
-	};
-	void process(EventConnection* ps)
+	}
+
+	void handle(GameConnection* conn, const Player::Ptr player) override
 	{
-	};
+		const auto roomPlayer = player->getRoomPlayer();
+
+		if (roomPlayer == nullptr)
+		{
+			return;
+		}
+
+		if (roomPlayer->isSpectating())
+		{
+			return;
+		}
+
+		const auto roomSessionPlayer = roomPlayer->getRoomSessionPlayer();
+
+		if (roomSessionPlayer == nullptr)
+		{
+			return;
+		}
+
+		if (roomSessionPlayer->isDead())
+		{
+			return;
+		}
+
+		roomSessionPlayer->setPosition({ xPos, yPos, zPos });
+	}
+
+	void process(EventConnection* ps) override
+	{
+		post<CGMoveReport>(ps);
+	}
 
 	TNL_DECLARE_CLASS(CGMoveReport);
 };
