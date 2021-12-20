7ac5ef7bd6fa5f189c48c1ccc6c5adba9093b870
Hinnie <hsibma02@gmail.com>
2021-12-20 18:47:43 +0100

    Spawn in first red board object, and send packet to players when they sync

--- src/essence.game/qpang/room/game_mode/modes/PlayerVsEnvironment.cpp
+++ src/essence.game/qpang/room/game_mode/modes/PlayerVsEnvironment.cpp
@@ -3,6 +3,8 @@
 #include "Room.h"
 #include "RoomSession.h"
 
+#include <qpang/room/tnl/net_events/server/gc_pve_object_init.hpp>
+#include <qpang/room/tnl/net_events/server/gc_pve_object_move.hpp>
 
 bool PlayerVsEnvironment::isMissionMode()
 {
@@ -26,7 +28,34 @@ void PlayerVsEnvironment::onApply(std::shared_ptr<Room> room)
 	GameMode::onApply(room);
 }
 
+void PlayerVsEnvironment::onStart(std::shared_ptr<RoomSession> roomSession)
+{
+	// 1st red board
+	roomSession->getObjectManager()->spawnObject({ 3, { -13.64f, -0.5f, -22.64f } });
+
+	GameMode::onStart(roomSession);
+}
+
+void PlayerVsEnvironment::onPlayerSync(std::shared_ptr<RoomSessionPlayer> session)
+{
+	// Send all spawned objects/npc/items
+	for (auto& object : session->getRoomSession()->getObjectManager()->getObjects())
+	{
+		session->send<GCPvEObjectInit>(object.second.type, object.first, object.second.position.x, object.second.position.y, object.second.position.z, 0);
+	}
+
+	GameMode::onPlayerSync(session);
+}
+
 void PlayerVsEnvironment::tick(const std::shared_ptr<RoomSession> roomSession)
 {
+	// TODO: Implement object logic somewhere here.
+	// BOARD NOTES:
+	// 1, from:
+	// { -13.64f, -0.5f, -22.64f }
+
+	// 1, to:
+	// { -1.4f, -0.5f, -22.64f }
+
 	GameMode::tick(roomSession);
 }
--- src/essence.game/qpang/room/game_mode/modes/PlayerVsEnvironment.h
+++ src/essence.game/qpang/room/game_mode/modes/PlayerVsEnvironment.h
@@ -9,5 +9,8 @@ public:
 	bool isTeamMode() override;
 	void onApply(std::shared_ptr<Room> room) override;
 
+	void onPlayerSync(std::shared_ptr<RoomSessionPlayer> session) override;
+	void onStart(std::shared_ptr<RoomSession> roomSession) override;
+
 	void tick(std::shared_ptr<RoomSession> roomSession) override;
 };
