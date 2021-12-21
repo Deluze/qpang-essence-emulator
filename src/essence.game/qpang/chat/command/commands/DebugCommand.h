79a5b4b09d52db5b44289526d8e04c7084d2f37e
Hinnie <hsibma02@gmail.com>
2021-12-21 23:45:32 +0100

    Event object debug command

--- src/essence.game/qpang/chat/command/commands/DebugCommand.h
+++ src/essence.game/qpang/chat/command/commands/DebugCommand.h
@@ -5,11 +5,26 @@
 #include "qpang/chat/command/Command.h"
 #include "qpang/room/RoomPlayer.h"
 
+#include "qpang/room/tnl/net_events/server/gc_pve_object_init.hpp"
+#include <qpang/room/tnl/net_events/server/gc_pve_object_move.hpp>
+#include <qpang/room/tnl/net_events/server/gc_pve_event_object.hpp>
+
 class DebugCommand final : public Command
 {
 public:
 	DebugCommand() : Command(3)
 	{
+	}
+
+	std::vector<CommandArgument*> getArguments() override
+	{
+		return {
+			Command::argTypes[Command::Validation::INTEGER],
+			Command::argTypes[Command::Validation::INTEGER]
+			/*Command::argTypes[Command::Validation::STRING],
+			Command::argTypes[Command::Validation::STRING],
+			Command::argTypes[Command::Validation::STRING]*/
+		};
 	}
 
 	void handle(const std::shared_ptr<Player> player, const std::vector<std::u16string>& args) override
@@ -33,5 +48,15 @@ public:
 		}
 
 		const auto roomSession = roomSessionPlayer->getRoomSession();
+
+		/*auto a1 = std::stof(std::string(args[0].begin(), args[0].end()));
+		auto a2 = std::stof(std::string(args[1].begin(), args[1].end()));
+		auto a3 = std::stof(std::string(args[2].begin(), args[2].end()));
+
+		roomSessionPlayer->send<GCPvEObjectInit>(5, 50, a1, a2, a3, 0);*/
+
+		roomSessionPlayer->send<GCPvEEventObject>(convertToInteger(args[0]), (U8)convertToInteger(args[1]));
+
+		//roomSessionPlayer->send<GCPvEObjectMove>(3, 39.1f, 0.f + convertToInteger(args[0]), -5.1f, 1000);
 	}
 };
