cd1f06cb87ea9ff62b09cffd6968cc68e4051d9c
Hinnie <hsibma02@gmail.com>
2021-12-22 16:04:35 +0100

    Added 4 escalators, and converted code to make use of the new EscalatorMoveInfo

--- src/essence.game/qpang/room/game_mode/modes/PlayerVsEnvironment.cpp
+++ src/essence.game/qpang/room/game_mode/modes/PlayerVsEnvironment.cpp
@@ -5,6 +5,7 @@
 
 #include "EscalatorObject.h"
 #include "BigGateObject.h"
+#include "SmallGateObject.h"
 #include "SwitchObject.h"
 
 #include <qpang/room/tnl/net_events/server/gc_pve_object_move.hpp>
@@ -33,11 +34,29 @@ void PlayerVsEnvironment::onApply(const std::shared_ptr<Room> room)
 
 void PlayerVsEnvironment::onStart(const std::shared_ptr<RoomSession> roomSession)
 {
-	roomSession->getObjectManager()->spawnObject(std::make_shared<EscalatorObject>(eEscalatorActionId::STAGE0_PIT1_ESCALATOR1, Position { -13.64f, -0.5f, -22.64f }));
-	roomSession->getObjectManager()->spawnObject(std::make_shared<EscalatorObject>(eEscalatorActionId::STAGE0_PIT2_ESCALATOR1, Position { 14.39f, -0.5f, -23.45f }));
+	auto moveInfo = EscalatorMoveInfo { Position { -13.64f, -0.5f, -22.64f }, Position { -1.4f, -0.5f, -22.64f }, 5, 1 };
+	roomSession->getObjectManager()->spawnObject(std::make_shared<EscalatorObject>(moveInfo));
 
-	uint32_t gateUid = roomSession->getObjectManager()->spawnObject(std::make_shared<BigGateObject>(Position{ 39.1f, 0.f, -5.1f }));
-	roomSession->getObjectManager()->spawnObject(std::make_unique<SwitchObject>(gateUid, Position{ 25.2f, -6.3f, 37.f }));
+	moveInfo = EscalatorMoveInfo{ Position { 14.39f, -0.5f, -23.45f }, Position { 36.56f, -0.5f, -23.45f }, 8, 1 };
+	roomSession->getObjectManager()->spawnObject(std::make_shared<EscalatorObject>(moveInfo));
+
+	moveInfo = EscalatorMoveInfo{ Position { -37.7f, -1.f, -3.4f }, Position { -37.7f, 1.6f, -3.4f }, 4, 0 };
+	roomSession->getObjectManager()->spawnObject(std::make_shared<EscalatorObject>(moveInfo));
+
+	moveInfo = EscalatorMoveInfo{ Position { -37.7f, 1.6f, -3.4f + 3.56f }, Position { -37.7f, -1.f, -3.4f + 3.56f }, 3, 0 };
+	roomSession->getObjectManager()->spawnObject(std::make_shared<EscalatorObject>(moveInfo));
+
+	moveInfo = EscalatorMoveInfo{ Position { -37.7f, -1.f, -3.4f + 3.56f * 2}, Position {-37.7f, 1.6f, -3.4f + 3.56f * 2}, 3, 0};
+	roomSession->getObjectManager()->spawnObject(std::make_shared<EscalatorObject>(moveInfo));
+
+	moveInfo = EscalatorMoveInfo{ Position { -37.7f, 1.6f, -3.4f + 3.56f * 3 }, Position { -37.7f, -1.f, -3.4f + 3.56f * 3 }, 2, 0 };
+	roomSession->getObjectManager()->spawnObject(std::make_shared<EscalatorObject>(moveInfo));
+
+	uint32_t bigGateUid = roomSession->getObjectManager()->spawnObject(std::make_shared<BigGateObject>(Position{ 39.1f, 0.f, -5.1f }));
+	roomSession->getObjectManager()->spawnObject(std::make_unique<SwitchObject>(bigGateUid, Position{ 25.2f, -6.3f, 37.f }));
+
+	uint32_t smallGateUid = roomSession->getObjectManager()->spawnObject(std::make_shared<SmallGateObject>(Position{ 24.61f, -6.3f, 7.17f }));
+	roomSession->getObjectManager()->spawnObject(std::make_unique<SwitchObject>(smallGateUid, Position{ 19.62f, -6.31f, 9.08f }));
 
 	GameMode::onStart(roomSession);
 }
