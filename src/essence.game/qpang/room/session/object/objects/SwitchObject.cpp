39ce7d1869aafd420266ba7e3899b86a59669faa
Hinnie <hsibma02@gmail.com>
2021-12-22 14:21:32 +0100

    SwitchObject logic simplified

--- src/essence.game/qpang/room/session/object/objects/SwitchObject.cpp
+++ src/essence.game/qpang/room/session/object/objects/SwitchObject.cpp
@@ -1,9 +1,10 @@
 #include "SwitchObject.h"
 
-#include "gc_pve_event_object.hpp"
+#include <qpang/room/tnl/net_events/server/gc_pve_door.hpp>
+
 #include "RoomSession.h"
 
-SwitchObject::SwitchObject(const uint32_t linkedGateId, const Position& position) : PveObject(eObjectType::SWITCH, position),
+SwitchObject::SwitchObject(uint32_t linkedGateId, const Position& position) : PveObject(eObjectType::SWITCH, position),
 	m_linkedGateId(linkedGateId)
 {
 }
@@ -15,14 +16,5 @@ void SwitchObject::tick(const std::shared_ptr<RoomSession>& roomSession)
 
 void SwitchObject::onEvent(const std::shared_ptr<RoomSession>& roomSession)
 {
-	const auto objectPtr = roomSession->getObjectManager()->findObjectByUid(m_linkedGateId);
-
-	if (objectPtr == nullptr)
-	{
-		return;
-	}
-
-	objectPtr->setShouldMove(true);
-
 	roomSession->getObjectManager()->openGate(m_linkedGateId);
 }
