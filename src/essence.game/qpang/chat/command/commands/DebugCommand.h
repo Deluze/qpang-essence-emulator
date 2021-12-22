bbab31bf5e124705f144fab3edb0a4b6188348ac
Hinnie <hsibma02@gmail.com>
2021-12-22 18:55:54 +0100

    Added npc spawn as debug command

--- src/essence.game/qpang/chat/command/commands/DebugCommand.h
+++ src/essence.game/qpang/chat/command/commands/DebugCommand.h
@@ -4,6 +4,7 @@
 #include "qpang/room/RoomPlayer.h"
 
 #include <qpang/room/tnl/net_events/server/gc_pve_object_init.hpp>
+#include <qpang/room/tnl/net_events/server/gc_pve_npc_init.hpp>
 
 class DebugCommand final : public Command
 {
@@ -17,7 +18,6 @@ public:
 		return {
 			//Command::argTypes[Command::Validation::INTEGER],
 			//Command::argTypes[Command::Validation::INTEGER]
-			Command::argTypes[Command::Validation::STRING],
 			Command::argTypes[Command::Validation::STRING],
 			Command::argTypes[Command::Validation::STRING],
 			Command::argTypes[Command::Validation::STRING]
@@ -49,9 +49,9 @@ public:
 		auto a1 = std::stof(std::string(args[0].begin(), args[0].end()));
 		auto a2 = std::stof(std::string(args[1].begin(), args[1].end()));
 		auto a3 = std::stof(std::string(args[2].begin(), args[2].end()));
-		auto a4 = std::stof(std::string(args[3].begin(), args[3].end()));
 
-		roomSessionPlayer->send<GCPvEObjectInit>(a1, 50, a2, a3, a4, 0);
+		//roomSessionPlayer->send<GCPvEObjectInit>(a1, 50, a2, a3, a4, 0);
 		//roomSessionPlayer->send<GCPvEObjectMove>(3, 39.1f, 0.f + convertToInteger(args[0]), -5.1f, 1000);
+		roomSession->relayPlaying<GCPvENpcInit>(eNpcType::EASY_SPY_CAM, 50, Position { 36, 0, -30 }, (U16)a1, (U8)a2, (U32)a3);
 	}
 };
