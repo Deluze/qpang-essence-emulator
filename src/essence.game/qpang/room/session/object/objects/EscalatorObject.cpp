fa399a223ab6517931c3a179d982abf9c0c17586
Hinnie <hsibma02@gmail.com>
2021-12-22 16:04:14 +0100

    Remove escalator ids, and add move info, that will be used inside PveObject::move

--- src/essence.game/qpang/room/session/object/objects/EscalatorObject.cpp
+++ src/essence.game/qpang/room/session/object/objects/EscalatorObject.cpp
@@ -2,16 +2,13 @@
 
 #include "RoomSession.h"
 
-EscalatorObject::EscalatorObject(const eEscalatorActionId actionId, const Position& position) : PveObject(eObjectType::MOVING_ESCALATOR, position)
+EscalatorObject::EscalatorObject(const EscalatorMoveInfo& moveInfo) : PveObject(eObjectType::MOVING_ESCALATOR, moveInfo.m_startPos)
 {
 	m_shouldMove = true;
-	m_actionId = actionId;
+	m_moveInfo = moveInfo;
 }
 
 void EscalatorObject::tick(const std::shared_ptr<RoomSession>& roomSession)
 {
-	if (m_actionId == eEscalatorActionId::STAGE0_PIT1_ESCALATOR1)
-		move(roomSession, m_position, { -1.4f, -0.5f, -22.64f }, 5);
-	else if (m_actionId == eEscalatorActionId::STAGE0_PIT2_ESCALATOR1)
-		move(roomSession, m_position, { 36.56f, -0.5f, -23.45f }, 8);
+	move(roomSession, m_moveInfo.m_startPos, m_moveInfo.m_endPos, m_moveInfo.m_durationSeconds, m_moveInfo.m_waitSeconds);
 }
--- src/essence.game/qpang/room/session/object/objects/EscalatorObject.h
+++ src/essence.game/qpang/room/session/object/objects/EscalatorObject.h
@@ -2,17 +2,27 @@
 
 #include "PveObject.h"
 
-enum class eEscalatorActionId {
-	STAGE0_PIT1_ESCALATOR1,
-	STAGE0_PIT2_ESCALATOR1
+struct EscalatorMoveInfo 
+{
+	// The starting position of the escalator
+	Position m_startPos;
+
+	// The ending position of the escalator
+	Position m_endPos;
+
+	// How long in seconds it should take to go from start to end, or from end to start
+	int m_durationSeconds;
+
+	// How long in seconds that the escalator should wait at the end pos before going back to the start pos, and vice-versa.
+	int m_waitSeconds;
 };
 
 class EscalatorObject final : public PveObject
 {
 public:
-	EscalatorObject(eEscalatorActionId actionId, const Position& position);
+	EscalatorObject(const EscalatorMoveInfo& moveInfo);
 
 	void tick(const std::shared_ptr<RoomSession>& roomSession) override;
 private:
-	eEscalatorActionId m_actionId;
+	EscalatorMoveInfo m_moveInfo;
 };
