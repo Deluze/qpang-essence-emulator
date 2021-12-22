a07007b80d5dcccc70bed8d71e5aed0177ff79e2
YestinJarrett <y.vastenouw@gmail.com>
2021-12-22 11:55:00 +0100

    Fix guaranteeType and eventDirection. Fix member types. Create constructor.

--- src/essence.game/qpang/room/tnl/net_events/server/gc_pve_area_trigger_init.hpp
+++ src/essence.game/qpang/room/tnl/net_events/server/gc_pve_area_trigger_init.hpp
@@ -3,20 +3,32 @@
 
 #include "GameNetEvent.h"
 
-class GCPvEAreaTriggerInit : public GameNetEvent
+class GCPvEAreaTriggerInit final : public GameNetEvent
 {
 	typedef NetEvent Parent;
 public:
-	GCPvEAreaTriggerInit() : GameNetEvent{ GC_PVEA_AREA_TRIGGER_INIT, NetEvent::GuaranteeType::Guaranteed, NetEvent::DirAny } {};
+	U32 unk_01; // 88	# ObjectUid maybe?
+	F32 unk_02; // 92	# PositionX maybe?
+	F32 unk_03; // 96	# PositionY maybe?
+	F32 unk_04; // 100  # PositionZ maybe?
+	F32 unk_05; // 104  # Unknown float.. something with position or direction perhaps?
+	U8 unk_06;  // 108
 
-	U32 unk_01;
-	U32 unk_02;
-	U32 unk_03;
-	U32 unk_04;
-	U32 unk_05;
-	U8 unk_06;
+	GCPvEAreaTriggerInit() : GameNetEvent{ GC_PVEA_AREA_TRIGGER_INIT, GuaranteedOrdered, DirServerToClient } {}
 
-	void pack(EventConnection* conn, BitStream* bstream)
+	GCPvEAreaTriggerInit(const uint32_t unk_01, const float unk_02, const float unk_03, const float unk_04, const  float unk_05, const uint8_t unk_06)
+		: GameNetEvent{ GC_PVEA_AREA_TRIGGER_INIT, GuaranteedOrdered, DirServerToClient },
+		unk_01(unk_01),
+		unk_02(unk_02),
+		unk_03(unk_03),
+		unk_04(unk_04),
+		unk_05(unk_05),
+		unk_06(unk_06)
+	{
+	}
+
+
+	void pack(EventConnection* conn, BitStream* bstream) override
 	{
 		bstream->write(unk_01);
 		bstream->write(unk_02);
@@ -24,9 +36,10 @@ public:
 		bstream->write(unk_04);
 		bstream->write(unk_05);
 		bstream->write(unk_06);
-	};
-	void unpack(EventConnection* conn, BitStream* bstream) {};
-	void process(EventConnection* ps) {};
+	}
+
+	void unpack(EventConnection* conn, BitStream* bstream) override {}
+	void process(EventConnection* ps) override {}
 
 	TNL_DECLARE_CLASS(GCPvEAreaTriggerInit);
 };
