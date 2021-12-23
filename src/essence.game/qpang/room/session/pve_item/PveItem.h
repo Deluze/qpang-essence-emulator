e56c593d94c5898617393e26e37ef2bef75673ee
YestinJarrett <y.vastenouw@gmail.com>
2021-12-23 02:51:24 +0100

    Add 3 extra item types (NONE, AMMO_CLIP and RED_MEDKIT).

--- src/essence.game/qpang/room/session/pve_item/PveItem.h
+++ src/essence.game/qpang/room/session/pve_item/PveItem.h
@@ -8,9 +8,12 @@ class RoomSession;
 
 enum class eItemType : uint32_t
 {
+	NONE = 0,
+	AMMO_CLIP = 1191182337,
+	RED_MEDKIT = 1191182338,
 	GOLDEN_COIN = 1191182352,
 	SILVER_COIN = 1191182353,
-	BRONZE_COIN = 1191182354,
+	BRONZE_COIN = 1191182354
 };
 
 class PveItem final
