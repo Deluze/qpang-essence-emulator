d9b3164078feaeb0820f7af3092590d83e79e3e4
Hinnie <hsibma02@gmail.com>
2021-12-25 18:02:39 +0100

    Remove old specific object classes, we're going to be using the db instead

--- src/essence.game/qpang/room/session/object/objects/BigGateObject.cpp
+++
@@ -1,7 +0,0 @@
-#include "BigGateObject.h"
-
-#include "RoomSession.h"
-
-BigGateObject::BigGateObject(const Position& position) : PveObject(eObjectType::BIG_GATE, position) {}
-
-void BigGateObject::tick(const std::shared_ptr<RoomSession>& roomSession) {}
--- src/essence.game/qpang/room/session/object/objects/SmallGateObject.cpp
+++
@@ -1,7 +0,0 @@
-#include "SmallGateObject.h"
-
-#include "RoomSession.h"
-
-SmallGateObject::SmallGateObject(const Position& position) : PveObject(eObjectType::SMALL_GATE, position) {}
-
-void SmallGateObject::tick(const std::shared_ptr<RoomSession>& roomSession) {}
