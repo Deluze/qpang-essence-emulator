734c0f31dc767e73ced11ba13598f1bfc9a2faf3
YestinJarrett <y.vastenouw@gmail.com>
2021-12-26 02:48:02 +0100

    Reversed cg_pve_hit_n2p and gc_pve_hit_n2p and provided a basic implementation for letting npcs deal damage and killing players.

--- src/essence.game/qpang/room/tnl/net_events/client/cg_pve_hit_n2p.hpp
+++ src/essence.game/qpang/room/tnl/net_events/client/cg_pve_hit_n2p.hpp
@@ -1,21 +1,24 @@
 #ifndef CG_PVE_HIT_N2P_H
 #define CG_PVE_HIT_N2P_H
 
+#include "CGPvEHitN2PData.h"
 #include "GameNetEvent.h"
+#include "gc_pve_hit_n2p.hpp"
+
 class CGPvEHitN2P final : public GameNetEvent
 {
 	typedef NetEvent Parent;
 public:
-	CGPvEHitN2P() : GameNetEvent{ CG_PVE_HIT_N2P, NetEvent::GuaranteeType::GuaranteedOrdered, NetEvent::DirAny } {}
+	CGPvEHitN2P() : GameNetEvent{ CG_PVE_HIT_N2P, GuaranteedOrdered, DirClientToServer } {}
 
-	U32 unk_01; // 88 
-	U32 unk_02; // 92
-	U32 unk_03; // 96
-	U32 unk_04; // 100
-	U32 unk_05; // 104
-	U32 unk_06; // 108
-	U32 unk_07; // 112
-	U32 unk_08; // 116
+	U32 npcUid; // 88
+	U32 npcBodyPartId; // 92
+	F32 impactPosX; // 96
+	F32 impactPosY; // 100
+	F32 impactPosZ; // 104
+	F32 impactOffsetPosX; // 108
+	F32 impactOffsetPosY; // 112
+	F32 impactOffsetPosZ; // 116
 	U8 unk_09;  // 120
 	U8 unk_10;  // 121
 	U64 unk_11; // 128?
@@ -24,23 +27,100 @@ public:
 
 	void unpack(EventConnection* conn, BitStream* bstream) override
 	{
-		bstream->read(&unk_01);
-		bstream->read(&unk_02);
-		bstream->read(&unk_03);
-		bstream->read(&unk_04);
-		bstream->read(&unk_05);
-		bstream->read(&unk_06);
-		bstream->read(&unk_07);
-		bstream->read(&unk_08);
+		bstream->read(&npcUid);
+		bstream->read(&npcBodyPartId);
+		bstream->read(&impactPosX);
+		bstream->read(&impactPosY);
+		bstream->read(&impactPosZ);
+		bstream->read(&impactOffsetPosX);
+		bstream->read(&impactOffsetPosY);
+		bstream->read(&impactOffsetPosZ);
 		bstream->read(&unk_09);
 		bstream->read(&unk_10);
 		bstream->read(&unk_11);
 	}
 
-	void handle(GameConnection* conn, Player::Ptr player) override
+	void handle(GameConnection* conn, const Player::Ptr player) override
 	{
-		// TODO: Handle CGPvEHitN2P.
-		std::cout << "Reveived an unhandled CGPvEHitN2P event." << std::endl;
+		/*std::cout << std::endl;
+		std::cout << "============ CG_PVE_HIT_N2P Report ============" << std::endl;
+		std::cout << "npcUid: " << (int)npcUid << std::endl;
+		std::cout << "bodyPartId: " << (int)npcBodyPartId << std::endl;
+		std::cout << "impactPosX: " << (float)impactPosX << std::endl;
+		std::cout << "impactPosY: " << (float)impactPosY << std::endl;
+		std::cout << "impactPosZ: " << (float)impactPosZ << std::endl;
+		std::cout << "impactOffsetPosX: " << (float)impactOffsetPosX << std::endl;
+		std::cout << "impactOffsetPosY: " << (float)impactOffsetPosY << std::endl;
+		std::cout << "impactOffsetPosZ: " << (float)impactOffsetPosZ << std::endl;
+		std::cout << "unk_09: " << (int)unk_09 << std::endl;
+		std::cout << "unk_10: " << (int)unk_10 << std::endl;
+		std::cout << "unk_11: " << (int)unk_11 << std::endl;
+		std::cout << "==============================================" << std::endl;*/
+
+		const auto roomPlayer = player->getRoomPlayer();
+
+		if (roomPlayer == nullptr)
+		{
+			return;
+		}
+
+		const auto roomSessionPlayer = roomPlayer->getRoomSessionPlayer();
+
+		if (roomSessionPlayer == nullptr)
+		{
+			return;
+		}
+
+		const auto roomSession = roomSessionPlayer->getRoomSession();
+
+		if (roomSession == nullptr)
+		{
+			return;
+		}
+
+		const auto npc = roomSession->getNpcManager()->findNpcByUid(npcUid);
+
+		if (npc == nullptr)
+		{
+			return;
+		}
+
+		// Check if the npc is dead. If the npc is dead they can not deal damage.
+		if (npc->isDead())
+		{
+			return;
+		}
+
+		// Check if player is dead, the player can not be dealt damage if they are dead.
+		if (roomSessionPlayer->isDead() || roomSessionPlayer->isInvincible())
+		{
+			return;
+		}
+
+		// Make sure the body part id from the client exists on the server.
+		if (!npc->hasBodyPart(npcBodyPartId))
+		{
+			return;
+		}
+
+		// TODO: Check if the player hit was valid (compare position between npc and player).
+
+		const auto impactPos = Position{ impactPosX, impactPosY, impactPosZ };
+		const auto impactOffsetPos = Position{ impactOffsetPosX, impactOffsetPosY, impactOffsetPosZ };
+
+		// TODO: Get the weaponid from the bodypart that was sent in.
+		const auto weaponUsed = Game::instance()->getWeaponManager()->get(npc->getWeaponItemId());
+
+		roomSessionPlayer->takeHealth(weaponUsed.damage, true);
+
+		roomSession->relayPlaying<GCPvEHitN2P>(npcUid, npcBodyPartId, impactPos, impactOffsetPos, unk_09, unk_10, 0, 0);
+
+		if (roomSessionPlayer->isDead())
+		{
+			// Update the killfeed.
+			roomSession->relayPlaying<GCGameState>(player->getId(), CGGameState::KILLFEED_ADD, weaponUsed.itemId, npcUid);
+			roomSession->getGameMode()->onPlayerDeathByNpc(roomSessionPlayer, npc);
+		}
 	}
 
 	void process(EventConnection* ps) override
--- src/essence.game/qpang/room/tnl/net_events/server/gc_pve_hit_n2p.hpp
+++ src/essence.game/qpang/room/tnl/net_events/server/gc_pve_hit_n2p.hpp
@@ -3,42 +3,62 @@
 
 #include "GameNetEvent.h"
 
-class GCPvEHitN2P : public GameNetEvent
+class GCPvEHitN2P final : public GameNetEvent
 {
 	typedef NetEvent Parent;
 public:
-	GCPvEHitN2P() : GameNetEvent{ GC_PVE_HIT_N2P, NetEvent::GuaranteeType::GuaranteedOrdered, NetEvent::DirAny } {};
+	U32 npcUid; // 88
+	U32 npcBodyPartId; // 92
+	F32 impactPosX; // 96
+	F32 impactPosY; // 100
+	F32 impactPosZ; // 104
+	F32 impactOffsetPosX; // 108
+	F32 impactOffsetPosY; // 112
+	F32 impactOffsetPosZ; // 116
 
-	U32 unk_01;
-	U32 unk_02;
-	U32 unk_03;
-	U32 unk_04;
-	U32 unk_05;
-	U32 unk_06;
-	U32 unk_07;
-	U32 unk_08;
-	U8 unk_09;
-	U8 unk_10;
-	U32 unk_11;
-	U32 unk_12;
+	U8 unk_09 = 0; // 120
+	U8 unk_10 = 0; // 121
 
-	void pack(EventConnection* conn, BitStream* bstream) 
+	U32 unk_11 = 0; // 124
+	U32 unk_12 = 0; // 128
+
+	GCPvEHitN2P() : GameNetEvent{ GC_PVE_HIT_N2P, GuaranteedOrdered, DirServerToClient } {}
+	GCPvEHitN2P(const uint32_t npcUid, const uint32_t npcBodyPartId, const Position impactPosition, const Position impactOffsetPosition, 
+		const uint8_t unk09,const uint8_t unk10, const uint32_t unk11, const uint32_t unk12)
+		: GameNetEvent{ GC_PVE_HIT_N2P, GuaranteedOrdered, DirServerToClient },
+		npcUid(npcUid),
+		npcBodyPartId(npcBodyPartId),
+		impactPosX(impactPosition.x),
+		impactPosY(impactPosition.y),
+		impactPosZ(impactPosition.z),
+		impactOffsetPosX(impactOffsetPosition.x),
+		impactOffsetPosY(impactOffsetPosition.y),
+		impactOffsetPosZ(impactOffsetPosition.z),
+		unk_09(unk09),
+		unk_10(unk10),
+		unk_11(unk11),
+		unk_12(unk12)
 	{
-		bstream->write(unk_01);			
-		bstream->write(unk_02);
-		bstream->write(unk_03);
-		bstream->write(unk_04);
-		bstream->write(unk_05);
-		bstream->write(unk_06);
-		bstream->write(unk_07);
-		bstream->write(unk_08);
+	}
+
+	void pack(EventConnection* conn, BitStream* bstream) override
+	{
+		bstream->write(npcUid);
+		bstream->write(npcBodyPartId);
+		bstream->write(impactPosX);
+		bstream->write(impactPosY);
+		bstream->write(impactPosZ);
+		bstream->write(impactOffsetPosX);
+		bstream->write(impactOffsetPosY);
+		bstream->write(impactOffsetPosZ);
 		bstream->write(unk_09);
 		bstream->write(unk_10);
 		bstream->write(unk_11);
 		bstream->write(unk_12);
-	};
-	void unpack(EventConnection* conn, BitStream* bstream) {};
-	void process(EventConnection* ps) {};
+	}
+
+	void unpack(EventConnection* conn, BitStream* bstream) override {}
+	void process(EventConnection* ps) override {}
 
 	TNL_DECLARE_CLASS(GCPvEHitN2P);
 };
