4ee0c7971009f7c29027495dc80b194ac06c4083
YestinJarrett <y.vastenouw@gmail.com>
2021-12-26 23:27:04 +0100

    Add method isBetweenAreaBounds.

--- src/essence.game/qpang/helpers/AABBHelper.h
+++ src/essence.game/qpang/helpers/AABBHelper.h
@@ -1,15 +1,18 @@
 #pragma once
 
 #include "Position.h"
+#include "PveArea.h"
 #include "Spawn.h"
 
 // ReSharper disable once CppInconsistentNaming
 class AABBHelper
 {
 public:
+	static bool isBetweenAreaBounds(PveArea::Bound minBound, PveArea::Bound maxBound, Position position);
+
 	static bool isPositionInBetweenBounds(const Position point, const Position minBound, const Position maxBound);
 	static bool IsPositionWithinSpawnRadius(Position position, Spawn spawn, float radius);
-	static long double AABBHelper::getDistanceBetweenPositions(const Position pos1, const Position pos2);
+	static long double getDistanceBetweenPositions(const Position pos1, const Position pos2);
 private:
 	static bool isNegative(const float value);
 	static bool isInRange(const float value, const float min, const float max);
