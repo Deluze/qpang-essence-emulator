27f1b323add3a88cd08a47b9c253366834790309
YestinJarrett <y.vastenouw@gmail.com>
2022-01-01 18:15:15 +0100

    Implemented npcs spawning on an interval (or as we know it "waves").

---
+++ src/essence.game/qpang/room/session/pve_wave/RoomSessionPveWaveManager.cpp
@@ -0,0 +1,88 @@
+#include "RoomSessionPveWaveManager.h"
+
+#include "Game.h"
+#include "RoomSession.h"
+
+void RoomSessionPveWaveManager::initialize(const std::shared_ptr<RoomSession>& roomSession)
+{
+	m_roomSession = roomSession;
+}
+
+void RoomSessionPveWaveManager::tick() const
+{
+	if (!m_isInitialized)
+	{
+		return;
+	}
+
+	const auto roomSession = m_roomSession.lock();
+
+	if (roomSession == nullptr)
+	{
+		return;
+	}
+
+	const auto timeLeft = roomSession->getTimeLeftInSeconds();
+
+	if (!m_waveIntervals.count(timeLeft))
+	{
+		// Time left does not occur in wave intervals so no npcs need to be spawned.
+		return;
+	}
+
+	const auto currentWaveNumber = m_waveIntervals.at(timeLeft);
+
+	if (!m_npcWaves.count(currentWaveNumber))
+	{
+		// Wave does not exist so therefor we can't spawn anything.
+		return;
+	}
+
+	const auto& npcs = m_npcWaves.at(currentWaveNumber);
+
+	for (const auto& npc : npcs)
+	{
+		roomSession->getNpcManager()->spawnNpc(std::make_shared<PveNpc>(npc));
+	}
+}
+
+void RoomSessionPveWaveManager::initializeWaves()
+{
+	const auto roomSession = m_roomSession.lock();
+
+	if (roomSession == nullptr)
+	{
+		return;
+	}
+
+	m_npcWaves.clear();
+
+	const auto mapId = roomSession->getPveRoundManager()->getMap();
+	const auto npcWaveData = Game::instance()->getPveManager()->getNpcWaveData(mapId);
+
+	for (const auto& [waveNumber, pveNpcWaveData] : npcWaveData)
+	{
+		for (const auto& data : pveNpcWaveData)
+		{
+			const auto [x, y, z] = data.initialPosition;
+
+			const auto pathFinder = (y < 0)
+				? roomSession->getUnderGroundPathfinder()
+				: roomSession->getAboveGroundPathfinder();
+
+			PathfinderCell spawnCell = {
+				pathFinder->getCellX(x),
+				pathFinder->getCellZ(z)
+			};
+
+			m_npcWaves[waveNumber].push_back(PveNpc(data, spawnCell));
+		}
+	}
+
+	m_isInitialized = true;
+}
+
+void RoomSessionPveWaveManager::removeAll()
+{
+	m_npcWaves.clear();
+}
---
+++ src/essence.game/qpang/room/session/pve_wave/RoomSessionPveWaveManager.h
@@ -0,0 +1,49 @@
+#pragma once
+
+#include <memory>
+#include <unordered_map>
+
+#include "PveNpc.h"
+
+class RoomSession;
+
+class RoomSessionPveWaveManager
+{
+public:
+	/**
+	 * \brief Initializes the pve wave manager with the room session.
+	 * \param roomSession The given room session.
+	 */
+	void initialize(const std::shared_ptr<RoomSession>& roomSession);
+
+	/**
+	 * \brief Ticks every tick.
+	 */
+	void tick() const;
+
+	/**
+	 * \brief Retrieves the waves + npc's from the PveManager.
+	 */
+	void initializeWaves();
+
+	void removeAll();
+private:
+	std::weak_ptr<RoomSession> m_roomSession;
+
+	std::unordered_map<uint32_t, std::vector<PveNpc>> m_npcWaves;
+	std::unordered_map<uint32_t, uint32_t> m_waveIntervals
+	{
+		std::make_pair(595, 1),
+		std::make_pair(540, 2),
+		std::make_pair(480, 3),
+		std::make_pair(420, 4),
+		std::make_pair(360, 5),
+		std::make_pair(300, 6),
+		std::make_pair(240, 7),
+		std::make_pair(180, 8),
+		std::make_pair(120, 9),
+		std::make_pair(60, 10)
+	};
+
+	bool m_isInitialized = false;
+};
