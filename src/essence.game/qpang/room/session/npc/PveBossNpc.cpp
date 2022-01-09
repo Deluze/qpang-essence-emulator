c0e2127586c4da478136f5f13d85ecca64696254
Hinnie <hsibma02@gmail.com>
2022-01-09 16:44:29 +0100

    Add boss npc class

---
+++ src/essence.game/qpang/room/session/npc/PveBossNpc.cpp
@@ -0,0 +1,32 @@
+#include "PveBossNpc.h"
+
+#include "RoomSession.h"
+
+#include <qpang/room/tnl/net_events/server/gc_pve_npc_init.hpp>
+#include <qpang/room/tnl/net_events/event_data/CGPvEHitNpcData.h>
+
+PveBossNpc::PveBossNpc(PveNpcData data, const PathfinderCell& spawnCell) :
+	PveNpc(data, spawnCell)
+{
+}
+
+void PveBossNpc::tick(const std::shared_ptr<RoomSession>& roomSession)
+{
+	PveNpc::tick(roomSession);
+}
+
+float PveBossNpc::calculateHitDamage(const CGPvEHitNpcData& data)
+{
+	return PveNpc::calculateHitDamage(data);
+	//return static_cast<float>(data.weaponUsed.damage);
+}
+
+void PveBossNpc::spawn(const std::shared_ptr<RoomSession>& roomSession) const
+{
+	roomSession->relayPlaying<GCPvENpcInit>(m_type, m_uid, m_initialPosition, m_initialRotation, m_health, true);
+}
+
+void PveBossNpc::spawn(const std::shared_ptr<RoomSessionPlayer>& roomSessionPlayer) const
+{
+	roomSessionPlayer->send<GCPvENpcInit>(m_type, m_uid, m_initialPosition, m_initialRotation, m_health, true);
+}
