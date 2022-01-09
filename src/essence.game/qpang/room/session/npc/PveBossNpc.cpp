d09ddf96282615c6e9968c068c9ecb06f3e7eac9
YestinJarrett <y.vastenouw@gmail.com>
2022-01-09 21:13:57 +0100

    Add basic special attack start and end implementation

--- src/essence.game/qpang/room/session/npc/PveBossNpc.cpp
+++ src/essence.game/qpang/room/session/npc/PveBossNpc.cpp
@@ -2,20 +2,42 @@
 
 #include "RoomSession.h"
 
-#include <qpang/room/tnl/net_events/server/gc_pve_npc_init.hpp>
 #include <utility>
 #include <qpang/room/tnl/net_events/event_data/CGPvEHitNpcData.h>
+#include <qpang/room/tnl/net_events/server/gc_pve_npc_init.hpp>
+
+#include "gc_master_log.hpp"
 
 PveBossNpc::PveBossNpc(PveNpcData data, const PathfinderCell& spawnCell) :
 	PveNpc(std::move(data), spawnCell),
 	m_isInSpecialAttack(false),
-	m_specialAttackDamageTreshold(5000.f)
+	m_specialAttackDamageTreshold(5000),
+	m_previousSpecialAttackHealth(data.baseHealth)
 {
 }
 
 void PveBossNpc::tick(const std::shared_ptr<RoomSession>& roomSession)
 {
 	PveNpc::tick(roomSession);
+
+	if (canStartSpecialAttack())
+	{
+		startSpecialAttack(roomSession);
+
+		return;
+	}
+
+	const auto currentTime = time(nullptr);
+
+	if (m_lastSpecialAttackTime == NULL)
+	{
+		return;
+	}
+
+	if ((m_lastSpecialAttackTime + m_specialAttackDuration) < currentTime)
+	{
+		endSpecialAttack(roomSession);
+	}
 }
 
 float PveBossNpc::calculateHitDamage(const CGPvEHitNpcData& data)
@@ -31,4 +53,27 @@ void PveBossNpc::spawn(const std::shared_ptr<RoomSession>& roomSession) const
 void PveBossNpc::spawn(const std::shared_ptr<RoomSessionPlayer>& roomSessionPlayer) const
 {
 	roomSessionPlayer->send<GCPvENpcInit>(m_type, m_uid, m_initialPosition, m_initialRotation, m_health, true);
+}
+
+bool PveBossNpc::canStartSpecialAttack() const
+{
+	return (!m_isInSpecialAttack && (m_health < (m_previousSpecialAttackHealth - m_specialAttackDamageTreshold)));
+}
+
+void PveBossNpc::startSpecialAttack(const std::shared_ptr<RoomSession>& roomSession)
+{
+	m_isInSpecialAttack = true;
+	m_previousSpecialAttackHealth = m_health;
+
+	m_lastSpecialAttackTime = time(nullptr);
+
+	roomSession->relayPlaying<GCMasterLog>(m_uid, 0, 0);
+}
+
+void PveBossNpc::endSpecialAttack(const std::shared_ptr<RoomSession>& roomSession)
+{
+	m_isInSpecialAttack = false;
+	m_lastSpecialAttackTime = NULL;
+
+	roomSession->relayPlaying<GCMasterLog>(m_uid, 0, 1);
 }
--- src/essence.game/qpang/room/session/npc/PveBossNpc.h
+++ src/essence.game/qpang/room/session/npc/PveBossNpc.h
@@ -14,11 +14,32 @@ public:
 
 	void tick(const std::shared_ptr<RoomSession>& roomSession) override;
 
-	float calculateHitDamage(const class CGPvEHitNpcData& data) override;
+	float calculateHitDamage(const CGPvEHitNpcData& data) override;
 
-	virtual void spawn(const std::shared_ptr<RoomSession>& roomSession) const override;
-	virtual void spawn(const std::shared_ptr<RoomSessionPlayer>& roomSessionPlayer) const override;
+	void spawn(const std::shared_ptr<RoomSession>& roomSession) const override;
+	void spawn(const std::shared_ptr<RoomSessionPlayer>& roomSessionPlayer) const override;
+
+	/**
+	 * \brief Indicates whether or not the boss can start its special attack.
+	 * \return true if the boss can start its special attack, otherwise false.
+	 */
+	[[nodiscard]]
+	bool canStartSpecialAttack() const;
+
+	/**
+	 * \brief Starts the special attack.
+	 * \param roomSession The room in session.
+	 */
+	void startSpecialAttack(const std::shared_ptr<RoomSession>& roomSession);
+
+	void endSpecialAttack(const std::shared_ptr<RoomSession>& roomSession);
+
 private:
 	bool m_isInSpecialAttack;
-	float m_specialAttackDamageTreshold;
+	uint16_t m_specialAttackDamageTreshold;
+
+	uint16_t m_previousSpecialAttackHealth;
+
+	uint32_t m_specialAttackDuration = 30;
+	uint32_t m_lastSpecialAttackTime = NULL;
 };
